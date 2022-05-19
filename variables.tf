74cac9dd3016ed0df0b450341d786fe696fa4fc0
Pablo de la Cuesta <pablo.delacuesta@outlook.com>
2022-05-19 19:59:51 +0200

    validation error

--- variables.tf
+++ variables.tf
@@ -5,10 +5,10 @@ variable "project" {
 variable "name" {
   description = "(Required) Name of the resource."
 
-  validation {
-    condition = can(regex("[a-z]([-a-z0-9]*[a-z0-9])", var.name))
-    error_message = "Name must be 1-63 characters long and match the regular expression"
-  }
+  # validation {
+  #   condition = can(regex("[a-z]([-a-z0-9]*[a-z0-9])", var.name))
+  #   error_message = "Name must be 1-63 characters long and match the regular expression"
+  # }
 }
 
 variable "description" {
